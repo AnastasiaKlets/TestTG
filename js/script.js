56712c075c660465362a06597bdcf0f29f70ad76
AnastasiaKlets <klecz.anastasia@gmail.com>
2024-03-06 14:47:44 +0300

    add: add adaptiv 5 block

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+    } else {
+        navbar.classList.remove("sticky");
+    }
+}
+
 $(document).ready(function($){
     $('.next-q').on('click', function(){
         var $el = $(this),
@@ -79,7 +97,7 @@ elements.forEach(elem => {
 });
 
 $('[data-modal=consultation]').on('click', function() {
-    $('.modal_back, #consultation').fadeIn(1);
+    $('.modal_back, #consultation').fadeIn('slow');
 });
 
 $('.modal_close').on('click', function() {
