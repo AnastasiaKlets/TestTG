b5570d8181eac09f264b6808d5bbeab139f3ed9b
AnastasiaKlets <klecz.anastasia@gmail.com>
2024-03-15 13:29:18 +0300

    change: struct

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 
 // When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+// window.onscroll = function() {myFunction()};
 
 // Get the navbar
-let navbar = document.getElementById("navbar");
-let content1 = document.getElementById("content1");
+// let navbar = document.getElementById("navbar");
+// let content1 = document.getElementById("content1");
 
 // Get the offset position of the navbar
-let sticky = navbar.offsetTop;
+// let sticky = navbar.offsetTop;
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 // function myFunction() {
